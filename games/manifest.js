1ef894a0bf6e2e3eb59d0724c17b6fe6bcf21a96
Nicole Selig <nicoleleeselig@gmail.com>
2026-05-16 09:29:51 -0400

    nicole selig's game

--- games/manifest.js
+++ games/manifest.js
@@ -1,3 +1,9 @@
 const GAMES = [
- 
+  {
+    "title": "Space Brick Buster",
+    "author": "Nicole Selig",
+    "path": "games/space-brick-buster/index.html",
+    "genre": "Brick Breaker",
+    "color": "Neon Cyber"
+  }
 ];
